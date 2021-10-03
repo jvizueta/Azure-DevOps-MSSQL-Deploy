08539af8987fab45ba409e14ed50f0af56b60951
admin <randym@dbmaestro.com>
2021-10-03 03:48:47 +0000

    fixed logging message for precheck

--- python-integ-lib/DBmAgent.py
+++ python-integ-lib/DBmAgent.py
@@ -20,7 +20,7 @@ class DBmAgent:
         logging.info(f"CREATING package.json MANIFEST FILE FOR PACKAGE FOLDER '{path_to_scripts_folder}'")
         logging.info(f"CREATEMANIFESTFILE START")
         cmd = f"java -jar \"{self.agent_path}\" -CreateManifestFile -PathToScriptsFolder \"{path_to_scripts_folder}\" -Operation {operation}"
-        logging.info("Cmd: " + cmd)
+        logging.info(f"Cmd: {cmd}")
         CommonUtils.run(cmd)
         logging.info(f"CREATEMANIFESTFILE END\n")
         logging.info(f"\n")
@@ -30,7 +30,7 @@ class DBmAgent:
         logging.info(f"GET PACKAGES OF '{project_name}/{env_name}' ENVIRONMENT AND PUT THEM IN FILE NAMED '{file_path}'")
         logging.info(f"GETENVPACKAGES START")
         cmd = f"java -jar \"{self.agent_path}\" -GetEnvPackages -ProjectName \"{project_name}\" -EnvName \"{env_name}\" -FilePath {file_path} -Server {self.dbm_server} -AuthType \"{self.dbm_user.auth_type}\" -UserName {self.dbm_user.username} -Password {self.dbm_user.password} -UseSSL {self.dbm_server.use_ssl}"
-        logging.info("Cmd: " + cmd)
+        logging.info(f"Cmd: {cmd}")
         CommonUtils.run(cmd)
         logging.info(f"GETENVPACKAGES END\n")
 
@@ -39,16 +39,16 @@ class DBmAgent:
         logging.info(f"PACKAGING FILE '{file_path}' INTO PROJECT '{project_name}'")
         logging.info(f"PACKAGE START")
         cmd = f"java -jar \"{self.agent_path}\" -Package -ProjectName \"{project_name}\" -FilePath {file_path} -Server {self.dbm_server} -AuthType \"{self.dbm_user.auth_type}\" -UserName {self.dbm_user.username} -Password {self.dbm_user.password} -UseSSL {self.dbm_server.use_ssl}"
-        logging.info("Cmd: " + cmd)
+        logging.info(f"Cmd: {cmd}")
         CommonUtils.run(cmd)
         logging.info(f"PACKAGE END\n")
 
     def precheck(self, project_name, package_name):
         logging.info(f"\n")
-        logging.info("PRECHECK PACKAGE '{packageName}' IN '{project_name}' PROJECT")
+        logging.info(f"PRECHECK PACKAGE '{package_name}' IN '{project_name}' PROJECT")
         logging.info(f"PRECHECK START")
         cmd = f"java -jar \"{self.agent_path}\" -PreCheck -ProjectName \"{project_name}\" -PackageName {package_name} -Server {self.dbm_server} -AuthType \"{self.dbm_user.auth_type}\" -UserName {self.dbm_user.username} -Password {self.dbm_user.password} -UseSSL {self.dbm_server.use_ssl}"
-        logging.info("Cmd: " + cmd)
+        logging.info(f"Cmd: {cmd}")
         CommonUtils.run(cmd)
         logging.info(f"PRECHECK END\n")
 
@@ -57,7 +57,7 @@ class DBmAgent:
         logging.info(f"UPGRADING '{project_name}/{env_name}' ENVIRONMENT TO PACKAGE '{package_name}'")
         logging.info(f"UPGRADE START")
         cmd = f"java -jar \"{self.agent_path}\" -Upgrade -ProjectName \"{project_name}\" -EnvName \"{env_name}\" -PackageName {package_name} -Server {self.dbm_server} -AuthType \"{self.dbm_user.auth_type}\" -UserName {self.dbm_user.username} -Password {self.dbm_user.password} -UseSSL {self.dbm_server.use_ssl}"
-        logging.info("Cmd: " + cmd)
+        logging.info(f"Cmd: {cmd}")
         CommonUtils.run(cmd)
         logging.info(f"UPGRADE END\n")
 
