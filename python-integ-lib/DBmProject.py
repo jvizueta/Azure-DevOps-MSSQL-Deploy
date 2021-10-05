45582bd53e833c7c524342e1ff6d3015fd09a12c
admin <randym@dbmaestro.com>
2021-10-05 20:30:24 +0000

    up to version limit for precheck and release source upgrade

--- python-integ-lib/DBmProject.py
+++ python-integ-lib/DBmProject.py
@@ -19,20 +19,24 @@ class DBmProject:
         latest_deployed_version = self.rs_env.get_latest_deployed_version()
         return CommonUtils.get_version_folders(self.root_folder, latest_deployed_version)
 
-    def upgrade_release_source_with_all_available_packages(self):
+    def upgrade_release_source_with_all_available_packages(self, up_to_version = None):
         available_package_names = self.get_available_package_names()
         for package_name in available_package_names:
-            package = DBmPackage(self, self.dbm_agent, package_name, self.root_folder, None)
-            package.create_or_update()
-            self.rs_env.upgrade(package.name)
+            package_version = CommonUtils.find_version(package_name)
+            if(up_to_version is None or not CommonUtils.is_version_greater_than(package_version, up_to_version)):
+                package = DBmPackage(self, self.dbm_agent, package_name, self.root_folder, None)
+                package.create_or_update()
+                self.rs_env.upgrade(package.name)
 
-    def precheck_all_available_packages_one_by_one(self):
+    def precheck_all_available_packages_one_by_one(self, up_to_version = None):
         available_package_names = self.get_available_package_names()
         latest_package = None
         for package_name in available_package_names:
-            package = DBmPackage(self, self.dbm_agent, package_name, self.root_folder, None)
-            package.create_or_update()
-            latest_package = package
+            package_version = CommonUtils.find_version(package_name)
+            if(up_to_version is None or not CommonUtils.is_version_greater_than(package_version, up_to_version)):
+                package = DBmPackage(self, self.dbm_agent, package_name, self.root_folder, None)
+                package.create_or_update()
+                latest_package = package
         if latest_package is not None:
             latest_package.precheck()
     
